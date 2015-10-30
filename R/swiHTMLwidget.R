ca3c6ad5f41263ac22a18f960513cb98248223d2
Duc-Quang Nguyen <ducquang.nguyen@gmail.com>
2015-10-30 16:41:24 +0100

    pre weekend

--- R/swiHTMLwidget.R
+++ R/swiHTMLwidget.R
@@ -2,17 +2,23 @@
 ##'
 ##' Helper functions to create a container for htmlwidget with javascript library files
 ##' @param widget.html,output.html character file path to the input widget html and the output reponsive html
-##' @param output a path to a folder where the output html file and depending js libraries will be saved
-##' @param source,author,h2,descr,h3 characters
+##' @param output a path to a folder where the output html file and the js library folder will be saved
+##' @param source,author,h2,descr,h3,footer characters
 ##' @export
 ##' @examples
 ##' \dontrun{
 ##' TO DO !!!!!!
 ##' }
-swi_widget <- function(widget.html, output.html = "parset_swi.html", output = ".",
+swi_widget <- function(
+  widget.html,
+  output.html = "parset_swi.html", 
+  output = ".",
   source = "source:",
-  author = "Duc-Quang Nguyen | swissinfo.ch", h2 = "title", descr = "descriptive text",
-  h3 = "subtitle") {
+  author = "Duc-Quang Nguyen | swissinfo.ch", 
+  h2 = "title", 
+  descr = "descriptive text",
+  h3 = "subtitle",
+  footer = "")  {
 
   output.html <- file.path(output, output.html)
   #change the output file name if already exists
@@ -21,6 +27,9 @@ swi_widget <- function(widget.html, output.html = "parset_swi.html", output = ".
     warning("\n Existing output html renamed to:", gsub("\\.html$", "_init\\.html", output.html), "\n")
   }
 
+  # check input html and library
+  if(!file.exists(widget.html)) { stop("\n", widget.html, " does not exist!")}
+    
   ## Load widget.html and get everything between the tag <div rChart highcharts> until the last <script>
   x <- readLines(widget.html)
   istart <- grep("htmlwidget_container", x)
@@ -46,11 +55,16 @@ swi_widget <- function(widget.html, output.html = "parset_swi.html", output = ".
 
   # add the footer: source & author
   cat('\n\n<!-- Source -->\n<div id="cite">', source, "|", author, "</div>")
+  
+  if(footer != "") {
+    cat('\n<!-- Footer -->\n<div id="footer">', footer, "</div>")
+  }
+  
   cat("\n\n\n</body>\n</html>")
 
   sink()
 
-  ## copy the javacript parset.css file
+  ## For parsetR: overwrite the javacsript d3.parset.css file if in the javacript folder
   original <- list.files("js", "d3.parsets.css", full.names = T, recursive = T)
   if(!identical(original, character(0))) {
     file.copy( from = list.files(system.file("extdata", package="swiRcharts"),
