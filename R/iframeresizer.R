3680f8690986e4e9dff89451ff5fe26973db9428
Duc-Quang Nguyen <ducquang.nguyen@gmail.com>
2016-06-21 13:25:32 +0200

    added iframeresizer

---
+++ R/iframeresizer.R
@@ -0,0 +1,10 @@
+##' html code to call SWI iframeresizer
+##'
+##' @export
+##' @seealso https://github.com/davidjbradshaw/iframe-resizer
+##' @examples
+##' \dontrun{
+##' iframeresizer
+##' }
+
+iframeresizer <- '<script type="text/javascript" src="https://www.swissinfo.ch/static/Themes/basic/js/vendor/iframeResizer.contentWindow.3.5.3.min.js"></script>'
